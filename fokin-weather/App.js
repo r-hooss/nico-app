c955b83a2094dc4cde89a7c9f529fe71e2910fdd
hooss <hoosshusu@gmail.com>
2020-05-08 20:58:15 +0900

    update code

--- fokin-weather/App.js
+++ fokin-weather/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Loading from "./loading";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  getLocation = async () => {
+    state = {
+      isLoading: true,
+    };
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return this.state.isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
